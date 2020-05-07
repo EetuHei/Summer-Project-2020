50ab7c91400bfca9490b55aa82b8553e0b1abc6c
EetuHei <t8heee01@students.oamk.fi>
2020-05-07 21:53:45 +0300

    minor change to where "i" gets additions to make the code more understandable

--- eloquent/2-program-structure/fizzbuzz/fizzbuzz.js
+++ eloquent/2-program-structure/fizzbuzz/fizzbuzz.js
@@ -1,3 +1,3 @@
-for (let i = 0; i < 100;) {
-  console.log((++i % 3 ? "" : "Fizz") + (i % 5 ? "" : "Buzz") || i)
+for (let i = 1; i <= 100; i++) {
+  console.log((i % 3 ? "" : "Fizz") + (i % 5 ? "" : "Buzz") || i)
 }
