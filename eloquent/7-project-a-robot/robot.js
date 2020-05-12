5a9b7172e2980cefd2545c00179a0698d5a91ada
EetuHei <t8heee01@students.oamk.fi>
2020-05-12 14:32:56 +0300

    measuring two robots done.

---
+++ eloquent/7-project-a-robot/robot.js
@@ -0,0 +1,196 @@
+const roads = [
+  "Alice's House-Bob's House",
+  "Alice's House-Cabin",
+  "Alice's House-Post Office",
+  "Bob's House-Town Hall",
+  "Daria's House-Ernie's House",
+  "Daria's House-Town Hall",
+  "Ernie's House-Grete's House",
+  "Grete's House-Farm",
+  "Grete's House-Shop",
+  "Marketplace-Farm",
+  "Marketplace-Post Office",
+  "Marketplace-Shop",
+  "Marketplace-Town Hall",
+  "Shop-Town Hall",
+];
+
+const mailRoute = [
+  "Alice's House",
+  "Cabin",
+  "Alice's House",
+  "Bob's House",
+  "Town Hall",
+  "Daria's House",
+  "Ernie's House",
+  "Grete's House",
+  "Shop",
+  "Grete's House",
+  "Farm",
+  "Marketplace",
+  "Post Office",
+];
+
+class VillageState {
+  constructor(place, parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
+
+  move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this;
+    } else {
+      let parcels = this.parcels
+        .map((p) => {
+          if (p.place != this.place) return p;
+          return { place: destination, address: p.address };
+        })
+        .filter((p) => p.place != p.address);
+      return new VillageState(destination, parcels);
+    }
+  }
+}
+
+function buildGraph(edges) {
+  let graph = Object.create(null);
+  function addEdge(from, to) {
+    if (graph[from] == null) {
+      graph[from] = [to];
+    } else {
+      graph[from].push(to);
+    }
+  }
+  for (let [from, to] of edges.map((r) => r.split("-"))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+  return graph;
+}
+
+const roadGraph = buildGraph(roads);
+
+let first = new VillageState("Post Office", [
+  { place: "Post Office", address: "Alice's House" },
+]);
+let next = first.move("Alice's House");
+
+// console.log(next.place);
+// // → Alice's House
+// console.log(next.parcels);
+// // → []
+// console.log(first.place);
+// // → Post Office
+
+function runRobot(state, robot, memory) {
+  for (let turn = 0; ; turn++) {
+    if (state.parcels.length == 0) {
+      console.log(`Done in ${turn} turns`);
+      break;
+    }
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
+  }
+}
+
+function randomPick(array) {
+  let choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+}
+
+function randomRobot(state) {
+  return { direction: randomPick(roadGraph[state.place]) };
+}
+
+function routeRobot(state, memory) {
+  if (memory.length == 0) {
+    memory = mailRoute;
+  }
+  return { direction: memory[0], memory: memory.slice(1) };
+}
+
+function findRoute(graph, from, to) {
+  let work = [{ at: from, route: [] }];
+  for (let i = 0; i < work.length; i++) {
+    let { at, route } = work[i];
+    for (let place of graph[at]) {
+      if (place == to) return route.concat(place);
+      if (!work.some((w) => w.at == place)) {
+        work.push({ at: place, route: route.concat(place) });
+      }
+    }
+  }
+}
+
+function goalOrientedRobot({ place, parcels }, route) {
+  if (route.length == 0) {
+    let parcel = parcels[0];
+    if (parcel.place != place) {
+      route = findRoute(roadGraph, place, parcel.place);
+    } else {
+      route = findRoute(roadGraph, place, parcel.address);
+    }
+  }
+  return { direction: route[0], memory: route.slice(1) };
+}
+
+/*
+Solution for Measuring a robot exercise
+*/
+
+// function to compare the robot efficiency between two robots.
+const compareRobots = (robot1, memory1, robot2, memory2) => {
+  // step counter
+  const getSteps = (state, robot, memory) => {
+    // loop to increase steps untill there is no parcels left
+    for (let steps = 0; ; steps++) {
+      if (state.parcels.length == 0) {
+        return steps;
+      }
+      // robot data
+      // action contains direction and memory data
+      let action = robot(state, memory);
+      state = state.move(action.direction);
+      memory = action.memory;
+    }
+  };
+
+  // set both robot steps to 0
+  let steps1 = 0,
+    steps2 = 0;
+
+  // loop to run the robots 100 times
+  // with random route in order to get better comparsion
+  for (let i = 0; i < 100; i++) {
+    let task = VillageState.random();
+    // call for steps to get total amount of steps from 100 runs
+    steps1 += getSteps(task, robot1, memory1);
+    steps2 += getSteps(task, robot2, memory2);
+  }
+  // log the total steps / 100 to get avg from 100 runs
+  console.log(`Robot 1 (route robot): ${steps1 / 100} steps`);
+  console.log(`Robot 2 (goal orientated robot): ${steps2 / 100} steps`);
+};
+
+/* 
+End of solution for Measuring a robot exercise
+*/
+
+VillageState.random = function (parcelCount = 5) {
+  let parcels = [];
+  for (let i = 0; i < parcelCount; i++) {
+    let address = randomPick(Object.keys(roadGraph));
+    let place;
+    do {
+      place = randomPick(Object.keys(roadGraph));
+    } while (place == address);
+    parcels.push({ place, address });
+  }
+  return new VillageState("Post Office", parcels);
+};
+
+// runRobot(VillageState.random(), routeRobot, []);
+//runRobotAnimation(VillageState.random(), routeRobot, []);
+compareRobots(routeRobot, [], goalOrientedRobot, []);
