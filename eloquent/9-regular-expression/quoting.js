1087279fc5c5794702e38028b3f6cc2acb72da95
EetuHei <t8heee01@students.oamk.fi>
2020-05-18 08:36:54 +0300

    fixed typo

--- eloquent/9-regular-expression/quoting.js
+++ eloquent/9-regular-expression/quoting.js
@@ -1,4 +1,4 @@
 let text = "'I'm the cook,' he said, 'it's my job.'";
 // Change this call.
 console.log(text.replace(/\W'|'\W|^'|\W $'|&'/g, "\""));
-// → "I'm the cook," he said, "it's my job."
+// → "I'm the cook," he said,"it's my job."
