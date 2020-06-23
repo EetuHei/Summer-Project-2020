54b41e28b729afa74c65dc0bdf3c17f2d9fae11e
EetuHei <t8heee01@students.oamk.fi>
2020-06-23 14:36:15 +0300

    refactored the main file, so I do not need to make new react project for each exercise set

--- fullstackopen/part1/src/index.js
+++ fullstackopen/part1/src/index.js
@@ -1,25 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Header from "./components/1.1-1.5/Header";
-import Content from "./components/1.1-1.5/Content";
-import Total from "./components/1.1-1.5/Total";
+import Basics from "./components/1.1-1.5/index.js"
+import Unicafe from "./components/1.6-/index";
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      { name: "Fundamentals of React", exercises: 10 },
-      { name: "Using props to pass data", exercises: 7 },
-      { name: "State of a component", exercises: 14 },
-    ],
-  };
-
   return (
-    <div>
-      <Header data={course} />
-      <Content data={course} />
-      <Total data={course} />
-    </div>
+    <>
+      {/* <Basics /> */}
+      <Unicafe />
+    </>
   );
 };
 
