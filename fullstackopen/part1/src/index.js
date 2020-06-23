187cf40394ae3592dec6779dbc04b25f73cebbfb
EetuHei <t8heee01@students.oamk.fi>
2020-06-23 14:44:25 +0300

    updated folder name

--- fullstackopen/part1/src/index.js
+++ fullstackopen/part1/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Basics from "./components/1.1-1.5/index.js"
-import Unicafe from "./components/1.6-/index";
+import Basics from "./components/1.1-1.5/index.js";
+import Unicafe from "./components/1.6-1.11/index";
 
 const App = () => {
   return (
