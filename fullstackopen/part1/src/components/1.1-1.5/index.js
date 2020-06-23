517adb75eaabc15575fd6d246764603ac455508a
EetuHei <t8heee01@students.oamk.fi>
2020-06-23 14:36:39 +0300

    index for basic exercises

---
+++ fullstackopen/part1/src/components/1.1-1.5/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Content from "./Content";
+import Header from "./Header";
+import Total from "./Total";
+
+const Basics = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      { name: "Fundamentals of React", exercises: 10 },
+      { name: "Using props to pass data", exercises: 7 },
+      { name: "State of a component", exercises: 14 },
+    ],
+  };
+
+  return (
+    <>
+      <div>
+        <Header data={course} />
+        <Content data={course} />
+        <Total data={course} />
+      </div>
+    </>
+  );
+};
+
+export default Basics;
