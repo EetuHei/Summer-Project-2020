6d3a29d6023a7cdbe37880b2b4193835f0c73e25
EetuHei <t8heee01@students.oamk.fi>
2020-06-24 13:19:06 +0300

    Course component

---
+++ fullstackopen/part1/src/components/2.1-2.5/components/Course/Content/Course.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../Header/Header";
+import Content from "./Content";
+import Total from "./Total";
+
+const Course = ({ courses }) => {
+  const { name, parts } = courses;
+  return (
+    <div>
+      <Header name={name} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+export default Course;
