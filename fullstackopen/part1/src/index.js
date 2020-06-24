a0ceb5eb5740155beea737efb659714ce772bcc4
EetuHei <t8heee01@students.oamk.fi>
2020-06-24 13:17:57 +0300

    added courseconents exercise

--- fullstackopen/part1/src/index.js
+++ fullstackopen/part1/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import Basics from "./components/1.1-1.5/index.js";
 import Unicafe from "./components/1.6-1.11/index";
 import Anecdotes from "./components/1.12-1.14/index"
+import CourseContents from "./components/2.1-2.5/components/Course/index"
+
 const App = () => {
   return (
     <>
       {/* <Basics /> */}
       {/* <Unicafe /> */}
-      <Anecdotes />
+      {/* <Anecdotes /> */}
+      <CourseContents />
     </>
   );
 };
