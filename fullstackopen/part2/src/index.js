077de0364c102ab0eb8937582b9fcfee84b1c4a6
EetuHei <t8heee01@students.oamk.fi>
2020-06-26 15:04:59 +0300

    added countries

--- fullstackopen/part2/src/index.js
+++ fullstackopen/part2/src/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Phonebook from "./components/2.6-2.10/index";
+import Countries from "./components/2.12-2.14/index"
 
 const App = () => {
 
   return (
     <>
       {/* <CourseContents /> */}
-      <Phonebook  />
+      {/* <Phonebook  /> */}
+      <Countries />
     </>
   );
 };
