fc76da71d7e864339bd1fd7c35f8363b6dca5aca
EetuHei <t8heee01@students.oamk.fi>
2020-06-29 11:20:34 +0300

    added button to delete person

--- fullstackopen/part2/src/components/2.6-2.10/components/Persons.js
+++ fullstackopen/part2/src/components/2.6-2.10/components/Persons.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-const Persons = ({ persons }) => {
+const Persons = ({ persons, handleDelete }) => {
   return (
     <>
       {persons.map((person) => (
         <p key={person.name}>
-          {person.name} {person.number}
+          {person.name} {person.number}{" "}
+          <button onClick={() => handleDelete(person.id)}>Delete</button>
         </p>
       ))}
     </>
