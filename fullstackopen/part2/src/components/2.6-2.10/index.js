62f0e1fc895eb7dcbc97e2031f7dd49e3cc0b3ad
EetuHei <t8heee01@students.oamk.fi>
2020-06-29 11:22:09 +0300

    modified hook to use services, added functionality delete persons and made reFetch state to fetch api data when data gets modified

--- fullstackopen/part2/src/components/2.6-2.10/index.js
+++ fullstackopen/part2/src/components/2.6-2.10/index.js
@@ -2,21 +2,22 @@ import React, { useState, useEffect } from "react";
 import Filter from "./components/Filter";
 import Form from "./components/Form";
 import Persons from "./components/Persons";
-import axios from "axios";
+import Services from "./services/Index";
 
 const Phonebook = () => {
   const [persons, setPersons] = useState("");
   const [newName, setNewName] = useState("");
   const [number, setNumber] = useState();
   const [filter, setFilter] = useState("");
+  const [reFetch, setReFetch] = useState(null);
 
   useEffect(() => {
-    console.log("effect");
-    axios.get("http://localhost:3001/persons").then((res) => {
-      console.log("promise fullified");
-      setPersons(res.data);
-    });
-  }, []);
+    Services.GetAll().then((res) => setPersons(res));
+    if (reFetch) {
+      Services.GetAll().then((res) => setPersons(res));
+      setReFetch(false);
+    }
+  }, [reFetch]);
   console.log("render", persons.length, "notes");
 
   const handleNameChange = (e) => setNewName(e.target.value);
@@ -32,12 +33,35 @@ const Phonebook = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    persons.map((person) => person.name).includes(newName)
-      ? alert(`${newName} is already added to the phonebook`)
-      : setPersons(persons.concat([{ name: newName, number: number }]));
+    if (persons.map((person) => person.name).includes(newName)) {
+      if (
+        window.confirm(
+          `${newName} is already added to phonebook, replace the old number with a new one ?`
+        )
+      ) {
+        let findPerson = persons.find((person) => person.name === newName);
+        Services.Update(findPerson.id, { name: newName, number: number });
+        setReFetch(true);
+      } else {
+      }
+    } else {
+      setPersons(
+        persons.concat(Services.Create({ name: newName, number: number }))
+      );
+      setReFetch(true);
+    }
   };
 
-  console.log(persons);
+  const handleDelete = (id) => {
+    let findPerson = persons.find((person) => person.id === id);
+
+    if (window.confirm(`Delete ${findPerson.name} ?`)) {
+      Services.Delete(id);
+      setReFetch(true);
+    } else {
+      alert(`${findPerson.name} has not been deleted.`);
+    }
+  };
 
   return (
     <>
@@ -52,7 +76,7 @@ const Phonebook = () => {
         handleSubmit={handleSubmit}
       />
       <h3>Numbers</h3>
-      <Persons persons={newPersons} />
+      <Persons persons={newPersons} handleDelete={handleDelete} />
     </>
   );
 };
