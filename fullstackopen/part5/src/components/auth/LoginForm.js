fd22b75efc5a0b21d8f9923c44e2df50b8027081
EetuHei <t8heee01@students.oamk.fi>
2020-07-10 15:09:09 +0300

    form for user to login

---
+++ fullstackopen/part5/src/components/auth/LoginForm.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+const LoginForm = ({ user, handleLogin }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const changeUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const changePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <>
+      {!user ? (
+        <>
+          <input
+            type="text"
+            value={username}
+            onChange={changeUsername}
+            placeholder="Username"
+          />{" "}
+          <input
+            type="password"
+            value={password}
+            onChange={changePassword}
+            placeholder="Password"
+          />{" "}
+          <button
+            onClick={() => {
+              handleLogin(username, password);
+            }}
+          >
+            Login
+          </button>
+        </>
+      ) : (
+        ""
+      )}
+    </>
+  );
+};
+
+export default LoginForm;
