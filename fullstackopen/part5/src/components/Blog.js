dd533cc14e8a349cdc34910781b9af3a8870d1af
EetuHei <t8heee01@students.oamk.fi>
2020-07-13 15:53:24 +0300

    refactored the blog component to have functionality to display more details of the blog

--- fullstackopen/part5/src/components/Blog.js
+++ fullstackopen/part5/src/components/Blog.js
@@ -1,8 +1,44 @@
-import React from "react";
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import React, { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [details, setDetails] = useState(false);
+
+  // TODO:
+  // get username by id and add the name of the poster in detailed view of the blog (5.7)
+  // move the blogs.map from app.js to under some other component
+
+  const buttonText = () => (details === true ? "hide" : "view");
+  const allDetails = { display: details ? "" : "none" };
+
+  let blogStyle = {
+    display: "",
+    marginTop: 10,
+    padding: 5,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div>
+      <div style={blogStyle}>
+        {blog.title}, by: {blog.author}
+        <button
+          style={{ marginLeft: "5px" }}
+          id="view"
+          onClick={() => setDetails(!details)}
+        >
+          {buttonText()}
+        </button>
+        <div style={allDetails} className="allDetails">
+          <div>
+            <a href="localhost:3000">{blog.url}</a>
+            <div>likes:{blog.likes}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
