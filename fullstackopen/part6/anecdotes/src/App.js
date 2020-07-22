1c18dbf9192d7f8458e538536d7dc377e31dfdb6
EetuHei <t8heee01@students.oamk.fi>
2020-07-22 15:53:49 +0300

    hook to fetch data from db and call for action

--- fullstackopen/part6/anecdotes/src/App.js
+++ fullstackopen/part6/anecdotes/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import { initAnecdotes } from './reducers/Actions'
+import { useDispatch } from 'react-redux'
+import anecdoteServices from './services/anecdoteServices'
+
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    anecdoteServices
+      .getAll()
+      .then((anecdotes) => dispatch(initAnecdotes(anecdotes)))
+  }, [dispatch])
+
   return (
     <>
       <h2>Anecdotes</h2>
