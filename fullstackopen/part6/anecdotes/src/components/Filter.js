7bf0c8b81f96b20f6055329699362c1de6ea3c90
EetuHei <t8heee01@students.oamk.fi>
2020-07-22 14:07:06 +0300

    filter input and call for action

---
+++ fullstackopen/part6/anecdotes/src/components/Filter.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { filterChange } from '../reducers/filterReducer'
+
+const Filter = () => {
+  const dispatch = useDispatch()
+
+  const handleChange = (e) => {
+    dispatch(filterChange(e.target.value))
+  }
+
+  const style = {
+    marginBottom: 10
+  }
+
+  return (
+    <div style={style}>
+      filter <input onChange={handleChange} />
+    </div>
+  )
+}
+
+export default Filter
