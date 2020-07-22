babaf7e26a57014c8602477842f8c01b6530021f
EetuHei <t8heee01@students.oamk.fi>
2020-07-22 14:04:33 +0300

    added init and reset notification when user votes on anecdote and added filtering of anecdotes

--- fullstackopen/part6/anecdotes/src/components/AnecdoteList.js
+++ fullstackopen/part6/anecdotes/src/components/AnecdoteList.js
@@ -1,21 +1,35 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { setVote } from '../reducers/Actions'
+import {
+  setVote,
+  initNotification,
+  resetNotification,
+} from '../reducers/Actions'
 
 const AnecdoteList = () => {
-  const anecdotes = useSelector((state) => state)
+  const anecdotes = useSelector((state) => state.anecdotes)
+  const filter = useSelector((state) => state.filter)
   const dispatch = useDispatch()
 
   const vote = (id) => {
     dispatch(setVote(id))
+    const findAnecdote = anecdotes.find((a) => a.id === id)
+    dispatch(initNotification(findAnecdote))
+    setTimeout(() => {
+      dispatch(resetNotification())
+    }, 5000)
   }
 
   let sortedAnecdotes = [...anecdotes]
   sortedAnecdotes.sort((a, b) => b.votes - a.votes)
 
+  const filteredAnecdotes = sortedAnecdotes.filter((anecdote) =>
+    anecdote.content.toLowerCase().includes(filter.toLowerCase())
+  )
+
   return (
     <div>
-      {sortedAnecdotes.map((anecdote) => (
+      {filteredAnecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
