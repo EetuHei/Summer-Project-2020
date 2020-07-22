98975539d727b8a92f03aa82a38d120cebb80bf0
EetuHei <t8heee01@students.oamk.fi>
2020-07-22 14:01:17 +0300

    added notification and filter components

--- fullstackopen/part6/anecdotes/src/App.js
+++ fullstackopen/part6/anecdotes/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
 import Filter from './components/Filter'
 const App = () => {
   return (
     <>
       <h2>Anecdotes</h2>
+      <Notification />
+      <Filter />
       <AnecdoteForm />
       <AnecdoteList />
     </>
