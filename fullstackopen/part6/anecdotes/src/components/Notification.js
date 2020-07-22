7783d27e3aaebf1896468db5e97708ee51c4ea4b
EetuHei <t8heee01@students.oamk.fi>
2020-07-22 14:08:15 +0300

    notification component, can handle multiple notifications

--- fullstackopen/part6/anecdotes/src/components/Notification.js
+++ fullstackopen/part6/anecdotes/src/components/Notification.js
@@ -1,16 +1,21 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Notification = () => {
+  const notification = useSelector((state) => state.notification.data)
+  console.log(notification, 'Im notification')
   const style = {
     border: 'solid',
     padding: 10,
-    borderWidth: 1
+    borderWidth: 1,
   }
-  return (
-    <div style={style}>
-      render here notification...
-    </div>
-  )
+
+  if (!notification) return <div></div>
+
+  if (notification.content)
+    return <div style={style}>you voted '{notification.content}'</div>
+
+  if (notification) return <div style={style}>'{notification}' was added</div>
 }
 
 export default Notification
