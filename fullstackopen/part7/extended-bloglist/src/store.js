40e9c9ce97cc75fd1d92533a014ed677a13e0daa
EetuHei <t8heee01@students.oamk.fi>
2020-07-29 16:07:02 +0300

    added blogs into combined reducers

--- fullstackopen/part7/extended-bloglist/src/store.js
+++ fullstackopen/part7/extended-bloglist/src/store.js
@@ -1,12 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import loginReducer from './reducers/loginReducer'
 import alertReducer from './reducers/alertReducer'
+import blogReducer from './reducers/blogReducer'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 const reducer = combineReducers({
   login: loginReducer,
   alert: alertReducer,
+  blogs: blogReducer,
 })
 
 const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
