83456a36ed919c8785bbb32d6ca83d1a43b4bd13
EetuHei <t8heee01@students.oamk.fi>
2020-07-30 12:42:14 +0300

    liking blog reducer

--- fullstackopen/part7/extended-bloglist/src/reducers/blogReducer.js
+++ fullstackopen/part7/extended-bloglist/src/reducers/blogReducer.js
@@ -8,6 +8,16 @@ const blogReducer = (state = [], action) => {
     case 'ADD_BLOG':
       return [...state, action.data]
 
+    case 'LIKE_BLOG':
+      const blogId = action.data.id
+      const blogToUpdate = state.find((a) => a.id === blogId)
+      console.log(blogToUpdate, 'finding the right blog to update')
+      const updateBlog = {
+        ...blogToUpdate,
+        likes: blogToUpdate.likes + 1,
+      }
+      return state.map((blog) => (blog.id !== blogId ? blog : updateBlog))
+
     case 'DELETE_BLOG':
       const id = action.data.id
       return state.filter((blog) => blog.id !== id)
