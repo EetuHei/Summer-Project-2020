8b9fc5d9084996cd102fa0bef776dece81c0afda
EetuHei <t8heee01@students.oamk.fi>
2020-07-30 09:08:32 +0300

    refactored adding blog to use custom hook and redux dispatchers

--- fullstackopen/part7/extended-bloglist/src/components/BlogForm.js
+++ fullstackopen/part7/extended-bloglist/src/components/BlogForm.js
@@ -1,53 +1,55 @@
 import React, { useState } from 'react'
+import { connect } from 'react-redux'
+import { useField } from '../hooks/useField'
+import { addBlog, setAlert } from '../reducers/Actions'
 
-const BlogForm = ({ user, handleBlogSubmit }) => {
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
+const BlogForm = (props) => {
+  const title = useField('text')
+  const author = useField('text')
+  const url = useField('text')
 
-  const changeTitle = (e) => {
-    setTitle(e.target.value)
+  const handleBlogSubmit = async (e) => {
+    e.preventDefault()
+    const res = await props.addBlog(title.value, author.value, url.value)
+    console.log('response from server: ', res)
+    if (!res || res.status === 400) {
+      props.setAlert(res.data.error, 5, 'red')
+    } else if (res.status === 500) {
+      props.setAlert(res.data.error)
+    } else if (res) {
+      props.setAlert(res, 5, 'green')
+    }
   }
 
-  const changeAuthor = (e) => {
-    setAuthor(e.target.value)
-  }
-
-  const changeUrl = (e) => {
-    setUrl(e.target.value)
-  }
-
-  if (user) {
+  if (props.userData.token) {
     return (
-      <form onSubmit={(e) => handleBlogSubmit(e, title, author, url)}>
+      <form onSubmit={handleBlogSubmit}>
         <h2>Create new</h2>
         <p>
-          Title:{' '}
-          <input type='text' id='title' value={title} onChange={changeTitle} />
+          Title: <input type='text' id='title' {...title} />
         </p>
         <p>
           Author:
-          <input
-            type='text'
-            id='author'
-            value={author}
-            onChange={changeAuthor}
-          />
+          <input type='text' id='author' value={author} {...author} />
         </p>
         <p>
-          Url: <input type='text' id='url' value={url} onChange={changeUrl} />
+          Url: <input type='text' id='url' {...url} />
         </p>
-        <button
-          id='submit'
-          type='submit'
-
-        >
+        <button id='submit' type='submit'>
           create
         </button>
-        <div></div>
       </form>
     )
   }
 }
 
-export default BlogForm
+const mapStateToProps = (state) => {
+  return { userData: state.auth }
+}
+
+const mapDispatchToProps = {
+  addBlog,
+  setAlert,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BlogForm)
