1cb702f4196068cdaba74348dad0bb7b2b555411
EetuHei <t8heee01@students.oamk.fi>
2020-07-30 09:07:27 +0300

    add blog case

--- fullstackopen/part7/extended-bloglist/src/reducers/blogReducer.js
+++ fullstackopen/part7/extended-bloglist/src/reducers/blogReducer.js
@@ -5,6 +5,8 @@ const blogReducer = (state = [], action) => {
   switch (action.type) {
     case 'INIT_BLOGS':
       return state.concat(action.data)
+    case 'ADD_BLOG':
+      return [...state, action.data]
     default:
       return state
   }
