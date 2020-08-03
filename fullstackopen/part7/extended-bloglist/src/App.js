1354024b35224a5621e1bdcc8f4eeda1997a8ce5
EetuHei <t8heee01@students.oamk.fi>
2020-08-03 13:13:14 +0300

    added routing for users list and single user and fetch for users data

--- fullstackopen/part7/extended-bloglist/src/App.js
+++ fullstackopen/part7/extended-bloglist/src/App.js
@@ -4,15 +4,24 @@ import { Switch, Route, Redirect, Link, useLocation } from 'react-router-dom'
 import LoginForm from './components/auth/LoginForm'
 import LogoutForm from './components/auth/LogoutForm'
 import BlogForm from './components/BlogForm'
+import UsersList from './components/UsersList'
+import User from './components/User'
 import Alert from './components/Alert'
 import Togglable from './components/Togglable'
 import { connect } from 'react-redux'
-import { loginUser, logoutUser, setAlert, initBlogs } from './reducers/Actions'
+import {
+  loginUser,
+  logoutUser,
+  setAlert,
+  initBlogs,
+  initUsers,
+} from './reducers/Actions'
 
 const App = (props) => {
   useEffect(() => {
     const fetchData = async () => {
       await props.initBlogs()
+      await props.initUsers()
     }
     fetchData()
   }, [])
@@ -46,14 +55,19 @@ const App = (props) => {
       <h2>blogs</h2>
       <Alert />
       <LogoutForm />
+      <Switch>
+        <Route path='/users' exact component={UsersList} />
+        <Route path='/users/:id' exact component={User} />
+        <Route path='/' exact>
+          <Togglable buttonLabel='create new blog'>
+            <BlogForm />
+          </Togglable>
 
-      <Togglable buttonLabel='create new blog'>
-        <BlogForm />
-      </Togglable>
-
-      {props.data.blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} />
-      ))}
+          {props.data.blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </Route>
+      </Switch>
     </div>
   )
 }
@@ -67,6 +81,7 @@ const mapDispatchToProps = {
   logoutUser,
   setAlert,
   initBlogs,
+  initUsers,
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App)
