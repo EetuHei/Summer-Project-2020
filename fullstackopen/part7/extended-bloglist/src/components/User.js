150463f6605d849ec7f26b03ec712a4e63eb8069
EetuHei <t8heee01@students.oamk.fi>
2020-08-03 17:29:18 +0300

    removed import that is not needed

--- fullstackopen/part7/extended-bloglist/src/components/User.js
+++ fullstackopen/part7/extended-bloglist/src/components/User.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Redirect, Route } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 
 const User = (props) => {
   const user = props.userData.filter(
