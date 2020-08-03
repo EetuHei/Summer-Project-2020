041abbe93508a50b9e81496e5b4e241c39e176c0
EetuHei <t8heee01@students.oamk.fi>
2020-08-03 13:13:57 +0300

    added functions to store to prevent state data loss on page refresh

--- fullstackopen/part7/extended-bloglist/src/store.js
+++ fullstackopen/part7/extended-bloglist/src/store.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux'
 import authReducer from './reducers/authReducer'
 import alertReducer from './reducers/alertReducer'
 import blogReducer from './reducers/blogReducer'
+import userReducer from './reducers/userReducer'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
@@ -9,8 +10,37 @@ const reducer = combineReducers({
   auth: authReducer,
   alert: alertReducer,
   blogs: blogReducer,
+  users: userReducer,
 })
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
+function saveToLocalStorage(state) {
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem('state', serializedState)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serializedState = localStorage.getItem('state')
+    if (serializedState === null) return undefined
+    return JSON.parse(serializedState)
+  } catch (e) {
+    console.log(e)
+    return undefined
+  }
+}
+
+const presistedState = loadFromLocalStorage()
+
+const store = createStore(
+  reducer,
+  presistedState,
+  composeWithDevTools(applyMiddleware(thunk))
+)
+
+store.subscribe(() => saveToLocalStorage(store.getState()))
 
 export default store
