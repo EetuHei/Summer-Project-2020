5627630995e5dac9943c37be25007eadd66d0950
EetuHei <t8heee01@students.oamk.fi>
2020-08-04 13:57:40 +0300

    refactoring and styling with bootstrap

--- fullstackopen/part7/extended-bloglist/src/components/Navbar.js
+++ fullstackopen/part7/extended-bloglist/src/components/Navbar.js
@@ -2,8 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { logoutUser } from '../reducers/Actions'
+import { Navbar, Nav, Button } from 'react-bootstrap'
 
-const Navbar = (props) => {
+const NavbarComponent = (props) => {
   const padding = {
     paddingRight: 5,
   }
@@ -19,15 +20,40 @@ const Navbar = (props) => {
   }
 
   return (
-    <div style={container}>
-      <Link style={padding} to='/'>
-        blogs
-      </Link>
-      <Link style={padding} to='/users'>
-        users
-      </Link>
-      {props.userData.name} logged in{' '}
-      <button onClick={() => props.logoutUser()}>logout</button>
+    <div style={{ padding: '5' }}>
+      <Navbar bg='dark' variant='dark' expand='md'>
+        <Nav className='mr-auto' style={{ color: '#fff' }}>
+          <Link
+            style={{
+              color: 'inherit',
+              marginRight: '5px',
+              textDecoration: 'underline',
+            }}
+            to='/'
+          >
+            blogs
+          </Link>
+          <Link
+            style={{
+              color: 'inherit',
+              marginRight: '5px',
+              textDecoration: 'underline',
+            }}
+            to='/users'
+          >
+            users
+          </Link>
+          {props.userData.name} logged in{' '}
+          <Button
+            size='sm'
+            variant='outline-secondary'
+            style={{ color: '#fff', marginLeft: '5px' }}
+            onClick={() => props.logoutUser()}
+          >
+            logout
+          </Button>
+        </Nav>
+      </Navbar>
     </div>
   )
 }
@@ -40,4 +66,4 @@ const mapDispatchToProps = {
   logoutUser,
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Navbar)
+export default connect(mapStateToProps, mapDispatchToProps)(NavbarComponent)
