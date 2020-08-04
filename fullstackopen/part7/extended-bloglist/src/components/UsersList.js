7a31ef2ff2b489420abbf32386d8a250593e3608
EetuHei <t8heee01@students.oamk.fi>
2020-08-04 14:00:20 +0300

    styling with bootstrap

--- fullstackopen/part7/extended-bloglist/src/components/UsersList.js
+++ fullstackopen/part7/extended-bloglist/src/components/UsersList.js
@@ -1,23 +1,35 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { Jumbotron } from 'react-bootstrap'
 
 const UsersList = (props) => {
   console.log('props in users: ', props)
 
   return (
     <div>
-      <h2>Users</h2>
-      <div>
-        <ul>blogs created</ul>
-        {props.usersData.map((data) => (
-          <div key={data.id}>
-            <Link to={`/users/${data.id}`}>
-              <p>{data.name}  {data.blogs.length}</p>
-            </Link>
-          </div>
-        ))}
-      </div>
+      <Jumbotron>
+        <h2>Users</h2>
+        <div>
+          <ul>blogs created</ul>
+          {props.usersData.map((data) => (
+            <div style={{ height: '20px' }} key={data.id}>
+              <Link
+                style={{
+                  color: 'inherit',
+                  marginRight: '5px',
+                  textDecoration: 'underline',
+                }}
+                to={`/users/${data.id}`}
+              >
+                <p>
+                  {data.name} {data.blogs.length}
+                </p>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </Jumbotron>
     </div>
   )
 }
