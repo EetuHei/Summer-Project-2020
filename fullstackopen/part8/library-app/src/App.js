33f1c1c3a34b7164541038ac574203d1d263e395
EetuHei <t8heee01@students.oamk.fi>
2020-08-06 13:26:10 +0300

    error message handling

--- fullstackopen/part8/library-app/src/App.js
+++ fullstackopen/part8/library-app/src/App.js
@@ -7,15 +7,29 @@ import { ALL_BOOKS } from './service/queries'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  const [errorMessage, setErrorMessage] = useState(null)
 
   const result = useQuery(ALL_BOOKS, {
     pollInterval: 2000,
   })
 
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
   if (result.loading) {
     return <div>loading...</div>
   }
 
+  const Notify = ({ errorMessage }) => {
+    if (!errorMessage) {
+      return null
+    }
+    return <div style={{ color: 'red' }}>{errorMessage}</div>
+  }
   console.log('response from apollo server: ', result.data.allBooks)
 
   return (
@@ -25,7 +39,7 @@ const App = () => {
         <button onClick={() => setPage('books')}>books</button>
         <button onClick={() => setPage('add')}>add book</button>
       </div>
-
+      <Notify errorMessage={errorMessage} />
       <Authors
         show={page === 'authors'}
         authors={result.data.allBooks.map((book) => book.author)}
@@ -33,7 +47,7 @@ const App = () => {
 
       <Books show={page === 'books'} books={result.data.allBooks} />
 
-      <NewBook show={page === 'add'} />
+      <NewBook show={page === 'add'} setError={notify} />
     </div>
   )
 }
