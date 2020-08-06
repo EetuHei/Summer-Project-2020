8d5289e09569ceda713056674628101da3d7ac33
EetuHei <t8heee01@students.oamk.fi>
2020-08-06 10:28:10 +0300

    added apollo provider

--- fullstackopen/part8/apollo-client/src/index.js
+++ fullstackopen/part8/apollo-client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client'
+import { ApolloClient, ApolloProvider, HttpLink, InMemoryCache, gql } from '@apollo/client'
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
@@ -29,4 +29,9 @@ client.query({ query }).then((response) => {
   console.log(response.data)
 })
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
