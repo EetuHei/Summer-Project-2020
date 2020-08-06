463045ba36d79861e60b085d36d96859c08e598a
EetuHei <t8heee01@students.oamk.fi>
2020-08-06 12:16:26 +0300

    refactored app to use apollo client setup

--- fullstackopen/part8/library-app/src/index.js
+++ fullstackopen/part8/library-app/src/index.js
@@ -1,5 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+  gql,
+} from '@apollo/client'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  }),
+})
+
+const query = gql`
+  query {
+    allBooks {
+      title
+      published
+      genres
+      id
+      author {
+        name
+        born
+        id
+        bookCount
+      }
+      id
+    }
+  }
+`
+
+client.query({ query }).then((response) => {
+  console.log(response.data)
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
