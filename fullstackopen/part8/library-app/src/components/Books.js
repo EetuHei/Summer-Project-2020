d095cf450865e1560a5df50d6e6282c897bd76c8
EetuHei <t8heee01@students.oamk.fi>
2020-08-10 13:40:53 +0300

    hook to make combined array from genres and remove dub entrys before setting them into state. Filter books based on selected genre.

--- fullstackopen/part8/library-app/src/components/Books.js
+++ fullstackopen/part8/library-app/src/components/Books.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import GenreFilter from './GenreFilter'
 
 const Books = (props) => {
+  const [books, setBooks] = useState(props.books)
+  const [genre, setGenre] = useState('')
+
+  useEffect(() => {
+    const genresArr = books.reduce((acc, book) => {
+      return acc.concat(book.genres)
+    }, [])
+
+    // remove duplicated genres from combined genres array
+    const removeDup = (arr) => {
+      return arr.reduce(
+        (unique, item) => (unique.includes(item) ? unique : [...unique, item]),
+        []
+      )
+    }
+    setGenre(removeDup(genresArr))
+  }, [books])
+
+  const filterByGenre = (genre) => {
+    setBooks(books.filter((book) => book.genres.includes(genre)))
+  }
+
+  const clearGenreFilter = () => {
+    setBooks([...props.books])
+  }
+
   if (!props.show) {
     return null
   }
-
-  const books = [...props.books]
 
   return (
     <div>
@@ -27,6 +52,12 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+
+      <GenreFilter
+        genres={genre}
+        filterByGenre={filterByGenre}
+        clear={clearGenreFilter}
+      />
     </div>
   )
 }
