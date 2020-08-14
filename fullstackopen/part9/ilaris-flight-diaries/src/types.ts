61b339853fdfd8b8c4847da65e5a398d8e3ba717
EetuHei <t8heee01@students.oamk.fi>
2020-08-14 14:34:19 +0300

    newdiary type

--- fullstackopen/part9/ilaris-flight-diaries/src/types.ts
+++ fullstackopen/part9/ilaris-flight-diaries/src/types.ts
@@ -1,7 +1,8 @@
 export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy'
 
 export type Visibility = 'great' | 'good' | 'ok' | 'poor'
-export type NonSensitiveDiaryEntry = Omit<DiaryEntry, 'comment'>;
+export type NonSensitiveDiaryEntry = Omit<DiaryEntry, 'comment'>
+export type NewDiaryEntry = Omit<DiaryEntry, 'id'>
 
 export interface DiaryEntry {
   id: number
