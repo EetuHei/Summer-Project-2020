a7131cb68cba4e9a2c6d2b3bfd6a91dc744276d4
EetuHei <t8heee01@students.oamk.fi>
2020-08-14 13:39:00 +0300

    diary router

--- fullstackopen/part9/ilaris-flight-diaries/index.ts
+++ fullstackopen/part9/ilaris-flight-diaries/index.ts
@@ -1,8 +1,12 @@
 import express from 'express'
 const app = express()
+import diaryRouter from './src/routes/diaries'
+
 app.use(express.json())
 
 const PORT = 8080
+
+app.use('/api/diaries', diaryRouter)
 
 app.get('/ping', (_req, res) => {
   console.log('someone pinged here')
