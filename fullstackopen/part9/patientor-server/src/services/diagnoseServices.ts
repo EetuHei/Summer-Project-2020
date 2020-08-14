1d9cb9b0284cfa16eb46a2b296d392fc11c7fd4e
EetuHei <t8heee01@students.oamk.fi>
2020-08-14 14:39:40 +0300

    diagnose services

---
+++ fullstackopen/part9/patientor-server/src/services/diagnoseServices.ts
@@ -0,0 +1,23 @@
+import diagnoses from '../data/diagnoses'
+import { NonSensitiveDiagnoseEntry, DiagnoseEntry } from '../types'
+
+const getEntries = (): DiagnoseEntry[] => {
+  return diagnoses
+}
+
+const getNonSensitiveEntries = (): NonSensitiveDiagnoseEntry[] => {
+  return diagnoses.map(({ code, name }) => ({
+    code,
+    name,
+  }))
+}
+
+const addEntry = () => {
+  return null
+}
+
+export default {
+  getEntries,
+  addEntry,
+  getNonSensitiveEntries,
+}
