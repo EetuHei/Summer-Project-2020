fd5d2dba617aa730fab7aa2aea5c201b190fa76a
EetuHei <t8heee01@students.oamk.fi>
2020-08-14 14:35:57 +0300

    endpoints to get diagnoses and patients

--- fullstackopen/part9/patientor-server/index.ts
+++ fullstackopen/part9/patientor-server/index.ts
@@ -1,10 +1,22 @@
 import express from 'express'
+import cors from 'cors'
 const app = express()
-app.use(express.json())
 
-const PORT = 8080
+import diagnoseRouter from './src/routes/diagnoses'
+import patientRouter from './src/routes/patients'
 
-app.get('/ping', (_req, res) => {
+app.use([
+  cors(),
+  express.urlencoded({ extended: true }),
+  express.json(),
+  express.static('build'),
+])
+
+app.use('/api/diagnoses', diagnoseRouter)
+app.use('/api/patients', patientRouter)
+const PORT = 3001
+
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here')
   res.send('pong')
 })
