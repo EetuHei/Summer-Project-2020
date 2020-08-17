4566decfe6621de7ccff8d485dacf465f05dc5b3
EetuHei <t8heee01@students.oamk.fi>
2020-08-17 13:35:20 +0300

    types boilerplate code

---
+++ fullstackopen/part9/courses-ts/src/types.tsx
@@ -0,0 +1,43 @@
+// new types
+export interface CoursePartBase {
+  name: string
+  exerciseCount: number
+}
+
+interface CoursePartOne extends CoursePartBase {
+  name: 'Fundamentals'
+  description: string
+}
+
+interface CoursePartTwo extends CoursePartBase {
+  name: 'Using props to pass data'
+  groupProjectCount: number
+}
+
+interface CoursePartThree extends CoursePartBase {
+  name: 'Deeper type usage'
+  description: string
+  exerciseSubmissionLink: string
+}
+
+export type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree
+
+// this is the new coursePart variable
+export const courseParts: CoursePart[] = [
+  {
+    name: 'Fundamentals',
+    exerciseCount: 10,
+    description: 'This is an awesome course part',
+  },
+  {
+    name: 'Using props to pass data',
+    exerciseCount: 7,
+    groupProjectCount: 3,
+  },
+  {
+    name: 'Deeper type usage',
+    exerciseCount: 14,
+    description: 'Confusing description',
+    exerciseSubmissionLink: 'https://fake-exercise-submit.made-up-url.dev',
+  },
+]
