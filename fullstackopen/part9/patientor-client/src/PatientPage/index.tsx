0c940180e3c699ded5a34e16ebeb84249357b539
EetuHei <t8heee01@students.oamk.fi>
2020-10-23 15:37:19 +0300

    Added view for patient based on id, fetches data from api

--- fullstackopen/part9/patientor-client/src/PatientPage/index.tsx
+++ fullstackopen/part9/patientor-client/src/PatientPage/index.tsx
@@ -1,11 +1,51 @@
 import React from 'react'
-import { useStateValue } from '../state'
+import axios from 'axios'
+import { apiBaseUrl } from '../constants'
+import { useParams } from 'react-router-dom'
+import { useStateValue, setPatientDetails } from '../state'
+import { Patient } from '../types'
 
 const PatientPage: React.FC = () => {
+  const [{ patient }, dispatch] = useStateValue()
+
+  // get param id
+  const {id} = useParams<{id: string}>()
+
+  // fetch patient data from api based on param id
+  React.useEffect(() => {
+    const fetchPatientData = async () => {
+      try {
+        const { data: patientDataById } = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${id}`
+        )
+        dispatch(setPatientDetails(patientDataById))
+      } catch (e) {
+        console.error(e)
+      }
+    }
+    if (!patient || patient?.id !== id) {
+      fetchPatientData()
+    }
+  }, [dispatch, id])
 
   return (
     <div>
-      <p>Patient page</p>
+      {patient ? (
+        <div>
+          {  Object.values(patient).map((data: Patient) => (
+           <>
+          <h1>{data.name}</h1>
+          <p>{data.ssn}</p>
+          <p>{data.occupation}</p>
+         </>
+  ))}
+       </div>
+       )
+       : 
+       (
+      <div>
+         <p>loading data...</p>
+      </div>)}
     </div>
   )
 }
