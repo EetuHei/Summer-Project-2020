426e69379f2c8a68ff5318a3ad37721157d2855e
EetuHei <t8heee01@students.oamk.fi>
2020-10-23 11:28:40 +0300

    boilerplate code

---
+++ fullstackopen/part9/patientor-client/src/PatientPage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useStateValue } from '../state'
+
+const PatientPage: React.FC = () => {
+
+  return (
+    <div>
+      <p>Patient page</p>
+    </div>
+  )
+}
+
+export default PatientPage
